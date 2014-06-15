7d41371add91be52249fdd3a94381bc58a6cbc62
Paul Reioux <reioux@gmail.com>
2014-06-15 17:39:41 -0500

    Revert "scheduler: compute time-average nr_running per run-queue"
    This reverts commit 7bff574c27b7b582f09431d8bbf64f1361264142.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -365,10 +365,6 @@ struct rq {
 #endif
 	int skip_clock_update;
 
-	/* time-based average load */
-	u64 nr_last_stamp;
-	unsigned int ave_nr_running;
-
 	/* capture load from *all* tasks on this cpu: */
 	struct load_weight load;
 	unsigned long nr_load_updates;
@@ -945,32 +941,24 @@ extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
 static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime) {}
 #endif
 
-/* 27 ~= 134217728ns = 134.2ms
- * 26 ~=  67108864ns =  67.1ms
- * 25 ~=  33554432ns =  33.5ms
- * 24 ~=  16777216ns =  16.8ms
- */
-#define NR_AVE_SCALE(x)                ((x) << FSHIFT)
-#define NR_AVE_PERIOD          (1 << NR_AVE_PERIOD_EXP)
-#define NR_AVE_DIV_PERIOD(x)   ((x) >> NR_AVE_PERIOD_EXP)
-
 #if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
-static inline void do_avg_nr_running(struct rq *rq)
+static inline unsigned int do_avg_nr_running(struct rq *rq)
 {
 
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 	unsigned int ave_nr_running = nr_stats->ave_nr_running;
 	s64 nr, deltax;
 
-	deltax = rq->clock_task - rq->nr_last_stamp;
-	rq->nr_last_stamp = rq->clock_task;
+	deltax = rq->clock_task - nr_stats->nr_last_stamp;
 	nr = NR_AVE_SCALE(rq->nr_running);
 
 	if (deltax > NR_AVE_PERIOD)
-		rq->ave_nr_running = nr;
+		ave_nr_running = nr;
 	else
-		rq->ave_nr_running +=
-			NR_AVE_DIV_PERIOD(deltax * (nr - rq->ave_nr_running));
+		ave_nr_running +=
+			NR_AVE_DIV_PERIOD(deltax * (nr - ave_nr_running));
+
+	return ave_nr_running;
 }
 #endif
 
@@ -1004,9 +992,6 @@ static inline void dec_nr_running(struct rq *rq)
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
 	nr_stats->nr_last_stamp = rq->clock_task;
-	rq->nr_last_stamp = rq->clock_task;
-	rq->nr_running--;
-	write_seqcount_end(&rq->ave_seqcnt);
 #endif
 	rq->nr_running--;
 #if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
